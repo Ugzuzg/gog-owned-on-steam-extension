ebdc82a538a63f2a6ab5e067490ec5404132338c
Jarasłaŭ Viktorčyk <ugzuzg@gmail.com>
2018-06-14 00:35:53 +0300

    feat: fetch ids from github

--- steamapi.js
+++
@@ -1,8 +0,0 @@
-const key = 'F6632EB18DA44A67BF2B446E6C476822';
-const endpoint = 'http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/';
-/*
-?key=XXXXXXXXXXXXXXXXX&steamid=76561197960434622&format=json
-*/
-fetch(`${endpoint}?key=${key}&steamid=76561197960434622&format=json`).then((response) => {
-  console.log(response.json());
-});
