3385aa15e5b1c8a4de694de07481610831bb7785
Jarasłaŭ Viktorčyk <ugzuzg@gmail.com>
2018-06-14 01:32:43 +0300

    chore: refactor options.js

--- src/options.js
+++ src/options.js
@@ -1,18 +1,14 @@
-function saveOptions(e) {
+const saveOptions = (e) => {
   browser.storage.sync.set({
-    steamid: document.querySelector('#steamid').value
+    steamid: document.querySelector('#steamid').value,
   });
   e.preventDefault();
-}
+};
 
-function restoreOptions() {
-  const storageItem = browser.storage.managed.get('steamid');
-  const gettingItem = browser.storage.sync.get('steamid');
-  gettingItem.then((res) => {
-    console.log(res);
-    document.querySelector('#steamid').value = res.steamid;
-  });
-}
+const restoreOptions = async () => {
+  const res = await browser.storage.sync.get('steamid');
+  document.querySelector('#steamid').value = res.steamid;
+};
 
 document.addEventListener('DOMContentLoaded', restoreOptions);
 document.querySelector('form').addEventListener('submit', saveOptions);
