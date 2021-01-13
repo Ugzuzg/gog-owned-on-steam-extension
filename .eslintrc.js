5810fb735df97f586cfd31130f46a10a788e0a07
Jarasłaŭ Viktorčyk <ugzuzg@gmail.com>
2021-01-13 00:06:11 -0100

    feat: manage dependencies with npm

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,14 @@
 module.exports = {
-  extends: 'airbnb-base',
+  extends: [
+    'airbnb-base',
+    'prettier',
+  ],
   env: {
     webextensions: true,
     browser: true,
-  }
+  },
+  globals: {
+    P: 'readonly',
+    _: 'readonly',
+  },
 };
