611af53046ddc0123de147cb370549f10c1e0a83
Jaryk <ugzuzg@gmail.com>
2024-09-26 19:06:25 +0200

    fix: update to new ITAD API (#20)

--- src/background.js
+++ src/background.js
@@ -1,25 +1,31 @@
 const steamEndpoint = 'https://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/';
 const steamApiKey = 'F6632EB18DA44A67BF2B446E6C476822';
-const itadKey = 'd1af465a5309c5664c366ff24e8441d3cbbcf38e';
-
-const fetchItadByGog = async ({ plains }) => {
-  const response = await fetch(
-    `https://api.isthereanydeal.com/v01/game/prices/?key=${itadKey}&shops=gog&plains=${plains.join(',')}`,
-  );
-  return (await response.json()).data;
-};
-
-const fetchItadPlains = async ({ gameIds }) => {
-  const response = await fetch(
-    `https://api.isthereanydeal.com/v01/game/plain/id/?key=${itadKey}&shop=steam&ids=${gameIds.join(',')}`,
-  );
-  const { data } = await response.json();
-  return data;
-};
 
 const fetchSteamGames = async ({ steamid }) => {
   const response = await fetch(`${steamEndpoint}?key=${steamApiKey}&steamid=${steamid}&format=json&include_appinfo=1`);
   return (await response.json()).response.games;
+};
+
+const lookupItadIdsByShopIds = async ({ shopId, gameIds }) => {
+  const response = await fetch(`https://api.isthereanydeal.com/lookup/id/shop/${shopId}/v1`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(gameIds),
+  });
+  return response.json();
+};
+
+const lookupShopIdsByItadIds = async ({ shopId, gameIds }) => {
+  const response = await fetch(`https://api.isthereanydeal.com/lookup/shop/${shopId}/id/v1`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(gameIds),
+  });
+  return response.json();
 };
 
 function connected(p) {
@@ -29,10 +35,10 @@ function connected(p) {
         switch (name) {
           case 'fetchSteamGames':
             return fetchSteamGames(params);
-          case 'fetchItadPlains':
-            return fetchItadPlains(params);
-          case 'fetchItadByGog':
-            return fetchItadByGog(params);
+          case 'lookupItadIdsByShopIds':
+            return lookupItadIdsByShopIds(params);
+          case 'lookupShopIdsByItadIds':
+            return lookupShopIdsByItadIds(params);
           default:
             throw new Error('unknown request');
         }
