9a046b47035b758a07f0079a9d5426b9ea029a79
Jarasłaŭ Viktorčyk <ugzuzg@gmail.com>
2024-09-26 20:25:44 +0200

    replace interval with MutationObserver

--- src/background.js
+++ src/background.js
@@ -1,23 +1,62 @@
 const steamEndpoint = 'https://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/';
 const steamApiKey = 'F6632EB18DA44A67BF2B446E6C476822';
 
+let steamGames = null;
+
 const fetchSteamGames = async ({ steamid }) => {
+  if (steamGames) return steamGames;
   const response = await fetch(`${steamEndpoint}?key=${steamApiKey}&steamid=${steamid}&format=json&include_appinfo=1`);
-  return (await response.json()).response.games;
+  steamGames = (await response.json()).response.games;
+  return steamGames;
 };
 
+const gog = '35';
+const steam = '61';
+let shopIdsToItadIds = {};
+
 const lookupItadIdsByShopIds = async ({ shopId, gameIds }) => {
+  const { hydratedResponse, missingIds } = gameIds.reduce(
+    (acc, id) => {
+      if (shopIdsToItadIds[shopId]?.[id]) {
+        acc.hydratedResponse[id] = shopIdsToItadIds[shopId][id];
+      } else {
+        acc.missingIds.push(id);
+      }
+      return acc;
+    },
+    { hydratedResponse: {}, missingIds: [] },
+  );
+
+  if (missingIds.length === 0) return hydratedResponse;
+
   const response = await fetch(`https://api.isthereanydeal.com/lookup/id/shop/${shopId}/v1`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
     },
-    body: JSON.stringify(gameIds),
+    body: JSON.stringify(missingIds),
   });
-  return response.json();
+  const data = await response.json();
+  shopIdsToItadIds = { ...shopIdsToItadIds, [shopId]: { ...data, ...hydratedResponse } };
+  return { ...hydratedResponse, ...data };
 };
 
+let itadIdsToShopIds = {};
 const lookupShopIdsByItadIds = async ({ shopId, gameIds }) => {
+  const { hydratedResponse, missingIds } = gameIds.reduce(
+    (acc, id) => {
+      if (id in (itadIdsToShopIds[shopId] ?? {})) {
+        acc.hydratedResponse[id] = itadIdsToShopIds[shopId][id];
+      } else {
+        acc.missingIds.push(id);
+      }
+      return acc;
+    },
+    { hydratedResponse: {}, missingIds: [] },
+  );
+
+  if (missingIds.length === 0) return hydratedResponse;
+
   const response = await fetch(`https://api.isthereanydeal.com/lookup/shop/${shopId}/id/v1`, {
     method: 'POST',
     headers: {
@@ -25,7 +64,10 @@ const lookupShopIdsByItadIds = async ({ shopId, gameIds }) => {
     },
     body: JSON.stringify(gameIds),
   });
-  return response.json();
+  const data = await response.json();
+
+  itadIdsToShopIds = { ...itadIdsToShopIds, [shopId]: { ...data, ...hydratedResponse } };
+  return { ...hydratedResponse, ...data };
 };
 
 function connected(p) {
